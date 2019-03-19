9ccb5df295c77205b745f2761b48604d360dd368
srop <por.srop@gmail.com>
2019-03-19 15:42:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<?
+<?php 
+header('Content-Type:application/json');
 function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
+    if($update["result"]["action"] == ""){
+        $fp = $parameters('request.log',$update["resutl"]["parameters"]["msg"]);
+        
         sendMessage(array(
             "source" => $update["result"]["source"],
             "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
+            "displayText" => "........TEXT HERE.........",
             "contextOut" => array()
         ));
     }
 }
  
 function sendMessage($parameters) {
+    $req_dump = print_r($parameters,true);
+    $fp = $parameters('request.log',$req_dump);
+    header('Content-Type:application/json');
     echo json_encode($parameters);
+    
 }
  
 $update_response = file_get_contents("php://input");
